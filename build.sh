1e7512363f115d70142c36e318b910f39b802041
cjh2423 <3452142707@qq.com>
2026-01-17 20:09:31 +0800

    第一次提交

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create build directory if it doesn't exist
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
+cd build
+
+# Run CMake to configure the project
+cmake ..
+
+# Build the project
+make
+
+echo "Build complete. Executable is located at build/hello"
---
+++ src/main.c
@@ -0,0 +1,10 @@
+/* main.c */
+
+#include <stdio.h>
+
+int main() {
+    puts("Hello World from C Language!");
+    puts("This runs on RV1126 with standard libraries.");
+    
+    return 0;
+}
